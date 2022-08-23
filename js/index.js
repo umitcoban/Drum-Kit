1c4829c36978cf83a2050b5bd2bd0b62115cc93b
Ümit Yasin Çoban <umityasincoban@gmail.com>
2022-08-23 23:59:32 +0300

    adding background and js function

---
+++ js/index.js
@@ -0,0 +1,42 @@
+var buttons = document.querySelectorAll(".drum");
+
+for(var i =0; i < buttons.length; i++){
+    buttons[i].addEventListener("click",handleClick);
+}
+
+function handleClick(){
+    // var audio = new Audio("sounds/tom-1.mp3");
+    // audio.play();
+    playDrumSet(this.textContent);
+}
+
+function playDrumSet(drumType){
+    var audio = new Audio();
+    switch(drumType){
+        case "w":
+            audio.src = "sounds/tom-1.mp3";
+            break;
+            case "a":
+                audio.src = "sounds/tom-1.mp3";
+                break;
+                case "s":
+                    audio.src = "sounds/tom-1.mp3";
+                    break;
+                    case "d":
+                        audio.src = "sounds/tom-1.mp3";
+                        break;
+                        case "j":
+                            audio.src = "sounds/tom-1.mp3";
+                            break;
+                            case "k":
+                                audio.src = "sounds/tom-1.mp3";
+                                break;
+                                case "l":
+                                    audio.src = "sounds/tom-1.mp3";
+                                    break;
+                                    default:
+
+    }
+ 
+    audio.play();
+}
